4ce6def470ee7f7b9a99b0121c0efb0b74fd3951
Malik Zubayer <mzubayeruh@gmail.com>
2024-07-07 15:57:48 +0600

    gtagmanager.init()

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,8 @@ import { Urbanist as Sans } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "./globals.css";
+import Head from "next/head";
+import Script from "next/script";
 
 const sans = Sans({ subsets: ["latin"] });
 
@@ -15,7 +17,22 @@ export default function RootLayout({ children }) {
   return (
     <React.StrictMode>
       <html lang="en">
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${process.env.NEXT_GTM_ID}');
+            `,
+          }} />
         <body className={sans.className}>
+          <noscript>
+            <iframe src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_GTM_ID}`} height="0" width="0" style="display:none;visibility:hidden"></iframe>
+          </noscript>
           <main className="flex flex-col min-h-screen">
             <Header />
             <div className="flex-1 flex flex-col">
